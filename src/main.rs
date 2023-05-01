f1605ec227a7764632deb0f357fab89e94e527ed
toddlewis-studio <todd.lewis.art@gmail.com>
2023-05-01 10:18:49 -0600

    Backend working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::fs::{relative, FileServer};
+use rocket::serde::{json::Json, Deserialize};
+
+// use solana_client::rpc_client::RpcClient;
+// use solana_sdk::pubkey::Pubkey;
+// use std::str::FromStr;
+
+extern crate proc_macro;
+
+#[macro_use]
+extern crate rocket;
+
+// #[post("/txmint/<pubkey>")]
+// fn txmint(pubkey: String) -> String {
+//     db::history(db::mint_path(pubkey.clone()), pubkey)
+// }
+
+#[launch]
+async fn rocket() -> _ {
+    rocket::build().mount("/", FileServer::from(relative!("/dist")))
+    // .mount("/", routes![balance, buytlc, buyalien, tx, txmint])
 }
